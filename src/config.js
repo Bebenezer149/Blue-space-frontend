27aba04bf69d3414f48b9d8dc7c029a514071eeb
Ebenezer Baidoo <bebenezer149@gmail.com>
2026-07-15 10:27:59 +0000

    Fix api

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // Use same-origin API calls on Vercel to avoid cross-origin issues.
 // Map in Vercel to an API route at: /api/*
-export const API_URL = "o/api";
+export const API_URL = "https://makola-2.onrender.com/api";
