eb8f802f28dbb57dbb3e1d6331dcdbafb1371517
Ebenezer Baidoo <bebenezer149@gmail.com>
2026-07-30 19:57:23 +0000

    Profile page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -25,11 +25,11 @@ function Header() {
     })
       .then((res) => res.json())
       .then((res) => {
-        console.log(res);
-        setImage(res.user.banner);
+        setImage(res.user.profile_picture);
       })
       .catch((err) => console.log(err));
   }, []);
+
   return (
     <div className="bg-white border-b border-gray-200 sticky top-0 z-40">
       <div className="max-w-7xl mx-auto p-4 flex justify-between items-center">
@@ -75,12 +75,12 @@ function Header() {
           </button>
 
           <Link to={"/profile"}>
-            <div className="h-14 w-14 border rounded-full border-slate-400 text-slate-400 cursor-pointer hover:text-blue-400 hover:border-blue-400">
+            <div className="h-14 w-14 border rounded-full border-slate-400 text-slate-400 cursor-pointer hover:text-blue-400 hover:border-blue-400 overflow-hidden flex items-center justify-center">
               {image ? (
                 <img
-                  className="h-full w-full rounded-full"
+                  className="h-full w-full rounded-full object-cover"
                   src={image}
-                  alt=""
+                  alt="Profile"
                 />
               ) : (
                 <svg
@@ -166,9 +166,32 @@ function Header() {
               Logout
             </button>
 
-            <div className="h-14 w-14 border rounded-full border-slate-400 text-slate-400 cursor-pointer hover:text-blue-400 hover:border-blue-400">
-              <img className="h-full w-full rounded-full" src={image} alt="" />
-            </div>
+            <Link to={"/profile"}>
+              <div className="h-14 w-14 border rounded-full border-slate-400 text-slate-400 cursor-pointer hover:text-blue-400 hover:border-blue-400 overflow-hidden flex items-center justify-center">
+                {image ? (
+                  <img
+                    className="h-full w-full rounded-full object-cover"
+                    src={image}
+                    alt="Profile"
+                  />
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                    />
+                  </svg>
+                )}
+              </div>
+            </Link>
           </div>
         </div>
       )}
